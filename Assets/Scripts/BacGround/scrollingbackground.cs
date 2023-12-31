40bf77094333155a1a5ecbead4040a332866532e
happyGamedev20 <121767726+happyGamedev20@users.noreply.github.com>
2023-12-30 22:25:32 -0500

    main Menu with deco and music
    made some nice main menu with pixel deco and with a no copy righted music

---
+++ Assets/Scripts/BacGround/scrollingbackground.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingbackground : MonoBehaviour
+{
+    public float speed;
+
+    [SerializeField] private Renderer bgRenderer;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
+    }
+}
