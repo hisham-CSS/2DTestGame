e3c072eee09a927da469c76868a0ed7cb45e4cb8
happyGamedev20 <121767726+happyGamedev20@users.noreply.github.com>
2023-12-30 22:59:22 -0500

    Made functionality to button
    made a functionality for button "Play tutorial"

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Test : MonoBehaviour
+{
+ public void LoadScene(int sceneId)
+ {
+  SceneManager.LoadScene(sceneId);
+ }
+}
